423691ff90821f974f1baf7cbe0bef5e106939f8
Hamon Riazy <hamon.riazy@judopayments.com>
2015-08-12 13:33:29 +0100

    created project

---
+++ Source/JudoKit.h
@@ -0,0 +1,14 @@
+//
+//  JudoKit.h
+//  JudoKit
+//
+//  Created by Hamon Riazy on 12/08/2015.
+//  Copyright © 2015 Judo Payments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double JudoKitVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char JudoKitVersionString[];
+
---
+++ Tests/JudoKitTests.swift
@@ -0,0 +1,24 @@
+//
+//  JudoKitTests.swift
+//  JudoKitTests
+//
+//  Created by Hamon Riazy on 12/08/2015.
+//  Copyright © 2015 Judo Payments. All rights reserved.
+//
+
+import XCTest
+@testable import JudoKit
+
+class JudoKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
