a14cd8ae380202d60ca09db991dc5990a3fae174
Hamon Riazy <hamon.riazy@judopayments.com>
2015-08-12 15:46:10 +0100

    updating JudoSecure framework integration

---
+++ Source/JudoKit.swift
@@ -0,0 +1,19 @@
+//
+//  JudoKit.swift
+//  JudoKit
+//
+//  Created by Hamon Riazy on 12/08/2015.
+//  Copyright © 2015 Judo Payments. All rights reserved.
+//
+
+import Foundation
+import JudoSecure
+import Judo
+
+public class JudoKit {
+    
+    public func someMethod() {
+        
+    }
+    
+}
