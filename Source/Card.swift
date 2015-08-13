fc9859858d3cbb32d845e9615d20ee4b5b082c2e
Ben Riazy <hamon.riazy@judopayments.com>
2015-08-13 17:34:33 +0100

    added luhn validation, refined detection and added beginning of cardPresentation method

--- Source/Card.swift
+++ Source/Card.swift
@@ -10,7 +10,42 @@ import Foundation
 import Judo
 
 
+
 public extension String {
+    
+    
+    func cardPresentationString() -> String? {
+        var strippedSelf = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+
+        if strippedSelf.characters.count == 0 || strippedSelf.cardNetwork() == .Unknown {
+            return nil
+        }
+        
+        var batchesArray = Array<String>()
+        
+        switch self.cardNetwork() {
+        case .Visa(.Debit), .Visa(.Credit), .Visa(.Unknown), .MasterCard(.Debit), .MasterCard(.Credit), .MasterCard(.Unknown), .Dankort, .JCB, .InstaPayment, .Discover where strippedSelf.characters.count == 16:
+            repeat {
+                let batch = strippedSelf.substringToIndex(advance(strippedSelf.startIndex, 4))
+                batchesArray.append(batch)
+                strippedSelf = strippedSelf.substringFromIndex(advance(strippedSelf.startIndex, 4))
+            } while strippedSelf.characters.count > 0
+            break
+        case .AMEX:
+            
+            break
+        default:
+            batchesArray.append(strippedSelf)
+        }
+        
+        var resultString = batchesArray.first
+        for batch in batchesArray.dropFirst() {
+            resultString = "\(resultString) \(batch)"
+        }
+        return resultString
+        
+    }
+    
     
     /**
     see https://en.wikipedia.org/wiki/Bank_card_number
@@ -22,17 +57,19 @@ public extension String {
     */
     func cardNetwork() -> CardNetwork {
         
+        let strippedSelf = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        
         // Card Networks that only have one prefix
         switch self {
-        case _ where self.beginsWith("1"):
+        case _ where strippedSelf.beginsWith("1"):
             return .UATP
-        case _ where self.beginsWith("4"):
+        case _ where strippedSelf.beginsWith("4"):
             return .Visa(.Unknown)
-        case _ where self.beginsWith("62"):
+        case _ where strippedSelf.beginsWith("62"):
             return .ChinaUnionPay
-        case _ where self.beginsWith("636"):
+        case _ where strippedSelf.beginsWith("636"):
             return .InterPayment
-        case _ where self.beginsWith("5019"):
+        case _ where strippedSelf.beginsWith("5019"):
             return .Dankort
         default:
             break
@@ -41,56 +78,56 @@ public extension String {
         // Card Networks with multiple different prefix
         // AMEX begins with 34 & 37
         for prefix in ["34", "37"] {
-            if self.beginsWith(prefix) {
+            if strippedSelf.beginsWith(prefix) {
                 return .AMEX
             }
         }
         
         // MasterCard begins with 51 - 55
         for prefix in (51...55).map({ String($0) }) {
-            if self.beginsWith(prefix) {
+            if strippedSelf.beginsWith(prefix) {
                 return .MasterCard(.Unknown)
             }
         }
         
         // future MasterCard range 2221-2720
         for prefix in (2221...2720).map({ String($0) }) {
-            if self.beginsWith(prefix) {
+            if strippedSelf.beginsWith(prefix) {
                 return .MasterCard(.Unknown)
             }
         }
         
         // Maestro has 50, 56-69
         for prefix in [56...69].map({ String($0) }) {
-            if self.beginsWith(prefix) {
+            if strippedSelf.beginsWith(prefix) {
                 return .Maestro
             }
         }
         
         // Diners Club 300-305 / 309 & 36, 38, 39
         for prefix in (300...305).map({ String($0) }) + ["36", "38", "39", "309"] {
-            if self.beginsWith(prefix) {
+            if strippedSelf.beginsWith(prefix) {
                 return .DinersClub
             }
         }
         
         // Discover 644-649
         for prefix in (644...649).map({ String($0) }) + ["65", "6011"] + (622126...622925).map({ String($0) }) {
-            if self.beginsWith(prefix) {
+            if strippedSelf.beginsWith(prefix) {
                 return .Discover
             }
         }
         
         // InstaPayment
         for prefix in (637...639).map({ String($0) }) {
-            if self.beginsWith(prefix) {
+            if strippedSelf.beginsWith(prefix) {
                 return .InstaPayment
             }
         }
         
         // JCB 3528-3589
         for prefix in (3528...3589).map({ String($0) }) {
-            if self.beginsWith(prefix) {
+            if strippedSelf.beginsWith(prefix) {
                 return .JCB
             }
         }
@@ -106,8 +143,14 @@ public extension String {
     - Returns: true if the string is a valid credit card number
     */
     func isCardNumberValid() -> Bool {
+        
         let network = self.cardNetwork()
-        let strippedSelfCount = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).characters.count
+        let strippedSelf = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        
+        guard strippedSelf.isLuhnValid() else { return false }
+
+        let strippedSelfCount = strippedSelf.characters.count
+        
         switch network {
         case .UATP, .AMEX:
             return strippedSelfCount == 15
@@ -127,11 +170,38 @@ public extension String {
     }
     
     
-    // helper method to check wether the string begins with another given string
+    /**
+    helper method to check wether the string begins with another given string
+    
+    - Parameter str: prefix string to compare
+    
+    - Returns: boolean indicating wether the prefix matches or not
+    */
     func beginsWith (str: String) -> Bool {
         if let range = self.rangeOfString(str) {
             return range.startIndex == self.startIndex
         }
         return false
     }
+    
+    
+    
+    func isLuhnValid() -> Bool {
+        guard self.isNumeric() else {
+            return false
+        }
+        let reversedInts = self.characters.reverse().map { Int(String($0)) }
+        return reversedInts.enumerate().reduce(0) { (sum, val) in
+            let odd = val.index % 2 == 1
+            return sum + (odd ? (val.element! == 9 ? 9 : (val.element! * 2) % 9) : val.element!)
+            } % 10 == 0
+    }
+    
+    
+    
+    func isNumeric() -> Bool {
+        return Double(self) != nil
+    }
+
+
 }
