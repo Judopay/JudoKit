eee4f2317b53e4ae24df64673b4bae71463a604e
Ben Riazy <hamon.riazy@judopayments.com>
2015-08-13 16:31:58 +0100

    added card validation method

--- Source/Card.swift
+++ Source/Card.swift
@@ -9,8 +9,8 @@
 import Foundation
 import Judo
 
-public struct Card {
-    
+
+public extension String {
     
     /**
     see https://en.wikipedia.org/wiki/Bank_card_number
@@ -20,110 +20,118 @@ public struct Card {
     
     - Returns: CardNetwork object
     */
-    static func cardNetwork(cardNumber: String) -> CardNetwork {
+    func cardNetwork() -> CardNetwork {
         
-        // need at least one characters to make an assumption about the network
-        if cardNumber.characters.count < 1 {
-            return .Unknown
-        }
-
-        // Visa
-        if cardNumber.characters.first == "4" {
-            return .Visa(.Unknown)
-        }
-        
-        // UATP
-        if cardNumber.characters.first == "1" {
+        // Card Networks that only have one prefix
+        switch self {
+        case _ where self.beginsWith("1"):
             return .UATP
-        }
-        
-        
-        // need at least two characters to make any additional assumptions if it does not start with a 4 (VISA) or 1 (UATP)
-        if cardNumber.characters.count < 2 {
-            return .Unknown
-        }
-        
-        // prepare
-        let firstTwo = cardNumber.substringToIndex(advance(cardNumber.startIndex, 2))
-        
-        // AMEX begins with 34 & 37
-        let amexCardPrefixRange = ["34", "37"]
-        if amexCardPrefixRange.contains(firstTwo) {
-            return .AMEX
-        }
-        
-        // some Diners Club with 36, 38 and 39
-        let dinersTwoPrefixRange = ["36", "38", "39"]
-        if dinersTwoPrefixRange.contains(firstTwo) {
-            return .DinersClub
-        }
-        
-        // China UnionPay starts with 62
-        if firstTwo == "62" {
+        case _ where self.beginsWith("4"):
+            return .Visa(.Unknown)
+        case _ where self.beginsWith("62"):
             return .ChinaUnionPay
+        case _ where self.beginsWith("636"):
+            return .InterPayment
+        case _ where self.beginsWith("5019"):
+            return .Dankort
+        default:
+            break
+        }
+        
+        // Card Networks with multiple different prefix
+        // AMEX begins with 34 & 37
+        for prefix in ["34", "37"] {
+            if self.beginsWith(prefix) {
+                return .AMEX
+            }
         }
         
         // MasterCard begins with 51 - 55
-        let masterCardPrefixRange = (51...55).map { String($0) }
-        if masterCardPrefixRange.contains(firstTwo) {
-            return .MasterCard(.Unknown)
+        for prefix in (51...55).map({ String($0) }) {
+            if self.beginsWith(prefix) {
+                return .MasterCard(.Unknown)
+            }
         }
         
-        // Discover prefix with two characters
-        if firstTwo == "65" {
-            return .Discover
+        // future MasterCard range 2221-2720
+        for prefix in (2221...2720).map({ String($0) }) {
+            if self.beginsWith(prefix) {
+                return .MasterCard(.Unknown)
+            }
         }
         
         // Maestro has 50, 56-69
-        let maestroCardPrefixRange = [56...69].map { String($0) }
-        if maestroCardPrefixRange.contains(firstTwo) {
-            return .Maestro
+        for prefix in [56...69].map({ String($0) }) {
+            if self.beginsWith(prefix) {
+                return .Maestro
+            }
         }
         
-        // need at least three characters to make any additional assumptions if it didnt match any of the previous checks
-        if cardNumber.characters.count < 3 {
-            return .Unknown
+        // Diners Club 300-305 / 309 & 36, 38, 39
+        for prefix in (300...305).map({ String($0) }) + ["36", "38", "39", "309"] {
+            if self.beginsWith(prefix) {
+                return .DinersClub
+            }
         }
-
-        // prepare
-        let firstThree = cardNumber.substringToIndex(advance(cardNumber.startIndex, 3))
-        
-        // other Diners Club 300-305 / 309
-        var dinersPrefixRange = (300...305).map { String($0) }
-        dinersPrefixRange.append("309")
-        if dinersPrefixRange.contains(firstThree) {
-            return .DinersClub
-        }
-        
         
         // Discover 644-649
-        let discoverPrefixRange = (644...649).map { String($0) }
-        if discoverPrefixRange.contains(firstThree) {
-            return .Discover
+        for prefix in (644...649).map({ String($0) }) + ["65", "6011"] + (622126...622925).map({ String($0) }) {
+            if self.beginsWith(prefix) {
+                return .Discover
+            }
         }
         
-        
-        // need at least four characters to make any additional assumptions if it didnt match any of the previous checks
-        if cardNumber.characters.count < 4 {
-            return .Unknown
+        // InstaPayment
+        for prefix in (637...639).map({ String($0) }) {
+            if self.beginsWith(prefix) {
+                return .InstaPayment
+            }
         }
         
-        // prepare
-        let firstFour = cardNumber.substringToIndex(advance(cardNumber.startIndex, 4))
-        
-        // Discover 6011
-        if firstFour == "6011" {
-            return .Discover
+        // JCB 3528-3589
+        for prefix in (3528...3589).map({ String($0) }) {
+            if self.beginsWith(prefix) {
+                return .JCB
+            }
         }
-        
-        // Discover 622126-622925
         
         // none of the patterns did match
         return .Unknown
     }
     
-    static func isCardNumberValid(cardNumber: String) -> Bool {
-        return true
+    
+    /**
+    check if the string is representing a valid credit card number
+    
+    - Returns: true if the string is a valid credit card number
+    */
+    func isCardNumberValid() -> Bool {
+        let network = self.cardNetwork()
+        let strippedSelfCount = self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).characters.count
+        switch network {
+        case .UATP, .AMEX:
+            return strippedSelfCount == 15
+        case .Visa(.Debit), .Visa(.Credit), .Visa(.Unknown):
+            return strippedSelfCount == 13 || strippedSelfCount == 16
+        case .MasterCard(.Debit), .MasterCard(.Credit), .MasterCard(.Unknown), .Dankort, .JCB, .InstaPayment, .Discover:
+            return strippedSelfCount == 16
+        case .Maestro:
+            return (12...19).contains(strippedSelfCount)
+        case .DinersClub:
+            return strippedSelfCount == 14
+        case .ChinaUnionPay, .InterPayment:
+            return (16...19).contains(strippedSelfCount)
+        case .Unknown:
+            return false
+        }
     }
     
+    
+    // helper method to check wether the string begins with another given string
+    func beginsWith (str: String) -> Bool {
+        if let range = self.rangeOfString(str) {
+            return range.startIndex == self.startIndex
+        }
+        return false
+    }
 }
