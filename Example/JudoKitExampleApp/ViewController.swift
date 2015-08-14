27ceecfd59f139ea1f9269d28fb58820f3075a76
Ben Riazy <hamon.riazy@judopayments.com>
2015-08-14 15:36:11 +0100

    added sample project and fixed some settings

---
+++ Example/JudoKitExampleApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  JudoKitExampleApp
+//
+//  Created by Hamon Riazy on 14/08/2015.
+//  Copyright © 2015 Judo Payments. All rights reserved.
+//
+
+import UIKit
+import JudoKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
