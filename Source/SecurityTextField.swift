05c23001f6f7298da44bcca402017692e1131e40
Ben Riazy <hamon.riazy@judopayments.com>
2015-09-01 17:26:48 +0100

    added textfield for cvc or cv2 texts

---
+++ Source/SecurityTextField.swift
@@ -0,0 +1,13 @@
+//
+//  SecurityTextField.swift
+//  JudoKit
+//
+//  Created by Hamon Riazy on 01/09/2015.
+//  Copyright © 2015 Judo Payments. All rights reserved.
+//
+
+import UIKit
+
+class SecurityTextField: UITextField {
+    
+}
