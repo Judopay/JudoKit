3063e03874b269b0012d8e69e432ecb4596f6e90
Ben Riazy <hamon.riazy@judopayments.com>
2015-09-01 11:32:11 +0100

    added copyright and view controller for boxed solution

--- Source/JPayViewController.swift
+++ Source/JPayViewController.swift
@@ -22,6 +22,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-import Foundation
-import Judo
+import UIKit
 
+class JPayViewController: UIViewController {
+    
+}
