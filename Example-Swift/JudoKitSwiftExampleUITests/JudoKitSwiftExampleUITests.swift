e7ab69528a56c300723e542f7e43fe45db339c9e
Ben Riazy <hamon.riazy@judopayments.com>
2015-11-23 16:55:58 +0000

    removed direct import into example apps - added new shield added UI Testing to Swift example

---
+++ Example-Swift/JudoKitSwiftExampleUITests/JudoKitSwiftExampleUITests.swift
@@ -0,0 +1,55 @@
+//
+//  JudoKitSwiftExampleUITests.swift
+//  JudoKitSwiftExampleUITests
+//
+//  Created by Hamon Riazy on 23/11/2015.
+//  Copyright © 2015 Judo Payments. All rights reserved.
+//
+
+import XCTest
+
+class JudoKitSwiftExampleUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+//        let app = XCUIApplication()
+        
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+        
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testVISAPayment() {
+        
+        let app = XCUIApplication()
+        app.tables.staticTexts["with default settings"].tap()
+        
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.textFields["Card number"].typeText("497600000000343")
+        
+        let expiryDateTextField = elementsQuery.textFields["Expiry date"]
+        expiryDateTextField.tap()
+        expiryDateTextField.typeText("6121")
+        
+        let cvv2TextField = elementsQuery.textFields["CVV2"]
+        cvv2TextField.tap()
+        cvv2TextField.typeText("5452")
+        app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.buttons["Pay"].tap()
+        app.buttons["Home"].tap()
+        
+        
+    }
+    
+}
