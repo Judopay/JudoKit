8a6df10fe580edca356219466ae1efba792cb3ba
Ashley Barrett <ashley.barrett@judopayments.com>
2016-09-13 10:31:58 +0100

    Pull latest JudoShield and upped version number to 6.2.2

--- Source/JudoKit.swift
+++ Source/JudoKit.swift
@@ -26,7 +26,7 @@ import Foundation
 import PassKit
 import JudoShield
 
-let JudoKitVersion = "6.2.1"
+let JudoKitVersion = "6.2.2"
 
 /**
  A method that checks if the device it is currently running on is jailbroken or not
