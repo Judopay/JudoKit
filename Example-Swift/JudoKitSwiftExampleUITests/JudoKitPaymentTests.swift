f76668b5671347fd96673574b439e68f5ee6d3c0
Ashley Barrett <ashley.barrett@judopayments.com>
2016-10-28 15:11:27 +0100

    added force touch method

--- Example-Swift/JudoKitSwiftExampleUITests/JudoKitPaymentTests.swift
+++ Example-Swift/JudoKitSwiftExampleUITests/JudoKitPaymentTests.swift
@@ -115,5 +115,16 @@ class JudoKitPaymentTests: XCTestCase {
         
         button.tap()
     }
-    
+}
+
+extension XCUIElement {
+    func forceTapElement() {
+        if self.isHittable {
+            self.tap()
+        }
+        else {
+            let coordinate: XCUICoordinate = self.coordinate(withNormalizedOffset: CGVector(dx: 0.0, dy: 0.0))
+            coordinate.tap()
+        }
+    }
 }
