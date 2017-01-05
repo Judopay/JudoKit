befe1417931bebfbc01db255efae6c858e1a288e
Ashley Barrett <ashley.barrett@judopayments.com>
2017-01-05 10:51:55 +0000

    fixed up failing tests

--- Example-Swift/JudoKitSwiftExampleUITests/JudoKit3DSPaymentTests.swift
+++ Example-Swift/JudoKitSwiftExampleUITests/JudoKit3DSPaymentTests.swift
@@ -155,7 +155,7 @@ class JudoKit3DSPaymentTests: XCTestCase {
         
         let startDateTextField = elementsQuery.textFields["Start date"]
         startDateTextField.tap()
-        startDateTextField.typeText("0107")
+        startDateTextField.typeText("0108")
         
         let expiryDateTextField = elementsQuery.textFields["Expiry date"]
         expiryDateTextField.tap()
