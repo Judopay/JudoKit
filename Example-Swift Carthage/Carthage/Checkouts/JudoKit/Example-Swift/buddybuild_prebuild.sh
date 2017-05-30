ff3fe1b862dcfe5e07c5e057701aefbe43dce544
james-lappin <james.lappin@judopayments.com>
2017-05-30 18:43:50 +0100

    Moved the sample app into another repo

--- Example-Swift Carthage/Carthage/Checkouts/JudoKit/Example-Swift/buddybuild_prebuild.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cp -f $BUDDYBUILD_SECURE_FILES/ExampleAppCredentials.swift $BUDDYBUILD_WORKSPACE/Example-Swift/JudoKitSwiftExample/ExampleAppCredentials.swift
--- Example-Swift Carthage/Carthage/Checkouts/JudoKit/Tests/JudoTestCaseConfig.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-let token = "<#YOUR TOKEN#>"
-let secret = "<#YOUR SECRET#>"
-let myJudoId = "<#YOUR JUDOID#>"
