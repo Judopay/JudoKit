1058a25a804913fbd8eaa9232bcf66ea60ecf7cf
Michael Hancock <mikehan99@hotmail.com>
2017-11-20 13:54:16 +0000

    updating version

--- Source/JudoKit.swift
+++ Source/JudoKit.swift
@@ -26,7 +26,7 @@ import Foundation
 import PassKit
 import DeviceDNA
 
-let JudoKitVersion = "6.2.17"
+let JudoKitVersion = "6.2.18"
 
 /**
  A method that checks if the device it is currently running on is jailbroken or not
